aeffdfc6637fd8690b3c9068d3575040d9c5b216
bastihz <41594266+BastiHz@users.noreply.github.com>
2019-08-24 22:29:37 +0200

    Make forest_fire() faster

--- R/forest_fire.R
+++ R/forest_fire.R
@@ -1,7 +1,3 @@
-# https://oeis.org/A229037
-# https://youtu.be/o8c4uYnnNnc?t=217
-
-
 #' Forest Fire
 #'
 #' Sequence of positive integers where each is chosen to be as small as possible
@@ -17,34 +13,28 @@
 #'   \href{https://youtu.be/o8c4uYnnNnc?t=217}{Amazing Graphs II
 #'   (including Star Wars)} (skip to 3:37).
 #'
-#' @note This implementation is quite slow. Be cautious with n > 10000.
-#'
 #' @examples
-#' ff <- forest_fire(2000)
-#' plot(ff)
+#' ff <- forest_fire(5000)
+#' plot(ff, pch = 16, col = "#00000070")
 #'
 #' @export
 forest_fire <- function(n) {
     stopifnot(n > 0)
-    s <- numeric()
-    for (k in seq_len(n)) {
-        i <- j <- 1
-        b <- numeric()
-        while (k - 2 * i > 0) {
-            b_new <- 2 * s[k-i] - s[k-2*i]
-            if (!b_new %in% b) {
-                b <- c(b, b_new)
-            }
-            i <- i + 1
-            while (j %in% b) {
-                b <- b[b != j]
-                j <- j + 1
-            }
+    if (n == 1) return(1)
+    if (n == 2) return(c(1, 1))
+    ff <- c(1, 1)
+    for (k in seq(2, n-1)) {
+        i <- seq(k, k/2+1, -1)
+        j <- seq(k-1, 1, -2)
+        forbidden <- unique(sort(ff[i] + ff[i] - ff[j]))
+        forbidden <- forbidden[forbidden > 0]
+        control <- seq_along(forbidden)
+        different <- which(forbidden != control)
+        if (length(different) > 0) {
+            ff <- c(ff, min(different))
+        } else {
+            ff <- c(ff, max(forbidden) + 1)
         }
-        s <- c(s, j)
     }
-    s
+    ff
 }
-
-# Implementation adapted from a python code example at https://oeis.org/A229037.
-# TODO: Make it faster.
