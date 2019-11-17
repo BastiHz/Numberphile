608f5c5e9e2e979fcf6bcfa6ea9361f70f0cf161
bastihz <41594266+BastiHz@users.noreply.github.com>
2019-11-17 23:04:06 +0100

    Add chaos_polygon()

--- R/chaos_game.R
+++ R/chaos_game.R
@@ -1,70 +1,69 @@
-# # Inspired by this Numberphile video:
-# # https://www.youtube.com/watch?v=kbKtFN71Lfs
-# #
-# # This script includes the polygons made from random points and the fern shown
-# # in the video. For more info on the fern see
-# # https://en.wikipedia.org/wiki/Barnsley_fern
-#
-#
-# chaos_polygon <- function(n = 3, move_distance = 0.5,
-#                           num_points = 10000, plot = TRUE, cex = 0.25, ...) {
-#     # Start with the corners of a regular polygon. Place a tracepoint somewhere
-#     # between those. Each step of the iteration choose a random corner and move
-#     # the tracepoint some distance towards that. Vary the number of corners and
-#     # the move distance to achieve different effects.
-#     # n = Number of corners.
-#     # ... = Further arguments which are passed to plot()
-#
-#     radians <- seq(0, by = 2 * pi / n, length.out = n)
-#     # Rotate the angles so that the bottom edge is horizontal. It just looks
-#     # nicer that way:
-#     radians <- radians - (pi / n) * (n - 1)
-#     corners <- data.frame(x = numeric(n), y = numeric(n))
-#     for (i in seq_len(n)) {
-#         corners$x[i] <- sin(radians[i])
-#         corners$y[i] <- cos(radians[i])
-#     }
-#
-#     # This version of the code below came to be after many iterations of
-#     # preformance improvements. At first I did everything using data frames
-#     # which took around 1 s for 1000 points. But using vectors like shown here
-#     # and creating the result data frame afterwards makes everything extremely
-#     # fast. Now it runs 1 million points in less than half a second. The new
-#     # bottleneck with so many points is the plot() function.
-#     trace_x <- corners$x[1]
-#     trace_y <- corners$y[1]
-#     result_x <- result_y <- numeric(num_points)
-#     targets <- sample(1:n, num_points, replace = TRUE)
-#     ct_x <- corners$x[targets]
-#     ct_y <- corners$y[targets]
-#     for (i in seq_len(num_points)) {
-#         trace_x <- trace_x - (trace_x - ct_x[i]) * move_distance
-#         trace_y <- trace_y - (trace_y - ct_y[i]) * move_distance
-#         result_x[i] <- trace_x
-#         result_y[i] <- trace_y
-#     }
-#     result <- data.frame(x = result_x, y = result_y)
-#
-#     if (!plot) return(result)
-#     opar <- par(no.readonly = TRUE)
-#     on.exit(par(opar))
-#     par(mar = rep(0, 4))
-#     plot(result, pch = 20, axes = TRUE, ann = FALSE, asp = 1, cex = cex, ...)
-# }
-#
-# # chaos_polygon()
-# # chaos_polygon(num_points = 50000)
-# # chaos_polygon(n = 4, move_distance = 0.55, num_points = 20000)
-# # chaos_polygon(n = 5, move_distance = 0.62, num_points = 30000)
-# # chaos_polygon(n = 6, move_distance = 2/3, num_points = 50000)
-# # chaos_polygon(n = 4, type = "l", num_points = 1000)
-# # chaos_polygon(30, move_distance = 0.91, num_points = 2000)
-# # chaos_polygon(2, move_distance = 0.7)
-# # chaos_polygon(move_distance = 1.5)
-# # chaos_polygon(move_distance = 1.99, num_points = 30000)
-#
-#
-# chaos_fern <- function(num_points = 10000, params = "barnsley",
+# TODO: Change param "n_vertices" to "vertices". Allow either a single integer
+# for a regular polyhedron or an object with an x and a y component as
+# user-defined points.
+
+
+#' Chaos Polygon
+#'
+#' Start with the vertices of a regular polygon. Randomly place a tracepoint at
+#' one of them. Each step of the iteration choose a random vertex and move the
+#' tracepoint some distance towards that.
+#'
+#' @param n_vertices The number of vertices.
+#' @param n_iterations The number of iterations.
+#' @param proportion The proportion of the distance to move from the point to
+#'   the target vertex.
+#'
+#' @references The Numberphile video featuring Ben Sparks and Brady Haran:
+#'   \href{https://youtu.be/kbKtFN71Lfs}{Chaos Game}.
+#'
+#' @examples
+#' p <- list(
+#'     chaos_polygon(),
+#'     chaos_polygon(4, 0.55),
+#'     chaos_polygon(5, 0.62),
+#'     chaos_polygon(6, 2/3),
+#'     chaos_polygon(30, 0.91),
+#'     chaos_polygon(proportion = 1.5)
+#' )
+#' par(mfrow = c(2, 3), mar = rep(0, 4))
+#' for (xy in p) {
+#'     plot(xy, pch = 20, axes = FALSE, ann = FALSE, asp = 1, cex = 0.25)
+#' }
+#'
+#' @export
+chaos_polygon <- function(n_vertices = 3,
+                          proportion = 0.5,
+                          n_iterations = 10000) {
+    # Construct the regular polyhedron. Rotate the thing so that the bottom
+    # edge is horizontal. It just looks nicer that way:
+    radians <- seq(0, by = 2 * pi / n_vertices, length.out = n_vertices)
+    radians <- radians - (pi / n_vertices) * (n_vertices - 1)
+    vertices <- data.frame(x = sin(radians), y = cos(radians))
+
+    # This version of the code below came to be after many iterations of
+    # preformance improvements. At first I did everything using data frames
+    # which took around 1 s for 1000 points. But using vectors like shown here
+    # and creating the result data frame afterwards makes everything extremely
+    # fast. Now it runs 1 million points in less than half a second. The new
+    # bottleneck with so many points is the plot() function.
+    tracepoint_x <- vertices$x[1]
+    tracepoint_y <- vertices$y[1]
+    result_x <- result_y <- numeric(n_iterations)
+    targets <- sample(1:n_vertices, n_iterations, replace = TRUE)
+    target_x <- vertices$x[targets]
+    target_y <- vertices$y[targets]
+    for (i in seq_len(n_iterations)) {
+        tracepoint_x <- tracepoint_x - (tracepoint_x - target_x[i]) * proportion
+        tracepoint_y <- tracepoint_y - (tracepoint_y - target_y[i]) * proportion
+        result_x[i] <- tracepoint_x
+        result_y[i] <- tracepoint_y
+    }
+    data.frame(x = result_x, y = result_y)
+}
+
+# Chaos Fern
+# chaos_fern <- function(n_iterations = 10000, params = "barnsley",
 #                        plot = TRUE, cex = 0.25, ...) {
 #     # params = Either the string "barnsley" or a data frame or matrix
 #     #     containing the parameters a, b, c, d, e, f and p.
@@ -84,17 +83,17 @@
 #     tracepoint <- c(0, 0)
 #     param_index <- sample(
 #         seq_len(nrow(params)),
-#         num_points - 1,  # -1 because the first point is always (0, 0)
+#         n_iterations - 1,  # -1 because the first point is always (0, 0)
 #         replace = TRUE,
 #         prob = params[, "p"]
 #     )
 #     result <- data.frame(
-#         x = numeric(num_points),
-#         y = numeric(num_points)
+#         x = numeric(n_iterations),
+#         y = numeric(n_iterations)
 #     )
 #     abcd_index <- match(c("a", "b", "c", "d"), colnames(params))
 #     ef_index <- match(c("e", "f"), colnames(params))
-#     for (i in seq_len(num_points - 1)) {
+#     for (i in seq_len(n_iterations - 1)) {
 #         k <- param_index[i]
 #         m <- matrix(params[k, abcd_index], nrow = 2, byrow = TRUE)
 #         tracepoint <- m %*% tracepoint + params[k, ef_index]
