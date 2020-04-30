3769c18e8e07e8b11ce768b20f641ad1e768c1de
Sebastian Henz <41594266+BastiHz@users.noreply.github.com>
2020-04-30 14:50:29 +0200

    Update look_and_say.R

--- R/look_and_say.R
+++ R/look_and_say.R
@@ -16,14 +16,33 @@ look_and_say <- function(seed = 1, n = 1, return_all = FALSE) {
 }
 
 
-# calculate conway's constant:
-foo <- look_and_say(1, n = 50, return_all = T)
-x <- numeric(length(foo) - 1)
-for (i in seq_along(foo)[-1]) {
-    x[i - 1] <- length(foo[[i]]) / length(foo[[i - 1]])
-}
-plot(x)
+# # calculate conway's constant:
+# foo <- look_and_say(1, n = 50, return_all = T)
+# x <- numeric(length(foo) - 1)
+# for (i in seq_along(foo)[-1]) {
+#     x[i - 1] <- length(foo[[i]]) / length(foo[[i - 1]])
+# }
+# plot(x)
+# 
+# 
+# # different seeds:
+# look_and_say(1:3, n = 10, return_all = T)
 
 
-# different seeds:
-look_and_say(1:3, n = 10, return_all = T)
+
+# # Version without rle():
+# seed = c(1, 2, 1, 1)
+# i <- 1
+# result <- numeric()
+# while (i <= length(seed)) {
+#     x <- seed[i]
+#     count <- 1
+#     while (i < length(seed) && x == seed[i + 1]) {
+#         count <- count + 1
+#         i <- i + 1
+#     }
+#     result <- c(result, count, x)
+#     i <- i + 1
+# }
+# result
+# seed <- result
