fe1b5d645e268df9639eec1bcac73aa830124639
bastihz <41594266+BastiHz@users.noreply.github.com>
2020-08-02 22:33:31 +0200

    Document and export look_and_say()

--- R/look_and_say.R
+++ R/look_and_say.R
@@ -1,20 +1,40 @@
-# look-and-say sequence: https://www.youtube.com/watch?v=ea7lJkEhytA
-# https://en.wikipedia.org/wiki/Look-and-say_sequence
-
-look_and_say <- function(seed = 1, n = 1, return_all = FALSE) {
+#' Look-and-Say Sequence
+#'
+#' A sequence which describes its previous term.
+#'
+#' @param n The number of iteration steps.
+#' @param seed The starting sequence.
+#' @param return_all Logical. Should all steps be returned? Defaults to
+#'   \code{TRUE}.
+#'
+#' @references This sequence in the On-Line Encyclopedia of Integer Sequences:
+#'   \href{https://oeis.org/A005150}{A005150}.
+#'
+#'   The Numberphile video featuring John Conway and Brady Haran:
+#'   \href{https://youtu.be/ea7lJkEhytA}{Look-and-Say Numbers (feat John
+#'   Conway)}.
+#'
+#' @return The final iteration of the sequence as a vector if \code{return_all}
+#'   is \code{FALSE}, otherwise a list of all steps of the sequence.
+#'
+#' @examples
+#' look_and_say(seed = 1, n = 7, return_all = TRUE)
+#'
+#' @export
+look_and_say <- function(n = 1, seed = 1, return_all = FALSE) {
+    stopifnot(n > 0)
     all_answers = list(seed)
     for (i in seq_len(n)) {
         x <- rle(seed)
         seed <- c(rbind(x$lengths, x$values))
         if (return_all) all_answers[[i + 1]] <- seed
     }
-    if (return_all) {
-        all_answers
-    } else {
-        seed
-    }
+    if (return_all) all_answers else seed
 }
 
+
+# TODO: clean this script
+# TODO: test the function
 
 # # calculate conway's constant:
 # foo <- look_and_say(1, n = 50, return_all = T)
@@ -23,8 +43,8 @@ look_and_say <- function(seed = 1, n = 1, return_all = FALSE) {
 #     x[i - 1] <- length(foo[[i]]) / length(foo[[i - 1]])
 # }
 # plot(x)
-# 
-# 
+#
+#
 # # different seeds:
 # look_and_say(1:3, n = 10, return_all = T)
 
